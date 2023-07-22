f37a1a8c090e73f5fb3ac4caf252bffcc09f33b6
Gabriel5489 <vegetaevil7@gmail.com>
2023-07-21 19:34:41 -0600

    Agregar archivos de proyecto.

---
+++ PruebasTVAzteca/Operaciones.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasTVAzteca
+{
+    public class Operaciones
+    {
+        static string cadena = "";
+        public static void Fibonacci(int n)
+        {
+            int auxiliar, a = 0, b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                auxiliar = a;
+                a = b;
+                b = auxiliar + a;
+                cadena += a + " ";
+            }
+            Console.WriteLine(cadena);
+        }
+
+        public static void SumaDigitos(int valor)
+        {
+            int suma = 0;
+            for (int i = 0; i < valor.ToString().Length; i++)
+            {
+                suma += Convert.ToInt32(valor.ToString().Substring(i, 1));
+            }
+            Console.WriteLine($"La suma del número {valor} es {suma}");
+        }
+
+        public static void NumerosPrimos(int inicio, int final)
+        {
+            for(int i = inicio; i <= final; i++){
+                if(EsPrimo(i)) cadena += i + " ";
+            }
+            Console.WriteLine(cadena);
+        }
+
+        public static bool EsPrimo(int valor) {
+            for (int i = 2; i < valor; i++)
+            {
+                if ((valor % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static void OrdenarPalabras(string oracion) {
+            List<string> palabras = oracion.Trim().Split(' ').ToList();
+            palabras.Sort();
+            string oracionOrdenada = String.Join(" ", palabras);
+            Console.WriteLine(oracionOrdenada);
+        }
+
+    }
+}
---
+++ PruebasTVAzteca/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasTVAzteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion = Presentacion();
+            int n, inicio, final;
+            while (opcion.ToUpper() != "F")
+            {
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Escribe la cantidad de numeros a mostrar");
+                        n = Convert.ToInt32(Console.ReadLine());
+                        Operaciones.Fibonacci(n);
+                        break;
+                    case "2":
+                        Console.WriteLine("Escribe un número");
+                        n = Convert.ToInt32(Console.ReadLine());
+                        Operaciones.SumaDigitos(n);
+                        break;
+                    case "3":
+                        Console.WriteLine("Escribe el inicio del rango");
+                        inicio = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Escribe el final del rango");
+                        final = Convert.ToInt32(Console.ReadLine());
+                        Operaciones.NumerosPrimos(inicio, final);
+                        break;
+                    case "4":
+                        Console.WriteLine("Escribe la frase a ordenar");
+                        string frase = Console.ReadLine();
+                        Operaciones.OrdenarPalabras(frase);
+                        break;
+                    default:
+                        Console.WriteLine("Elije una opción de la lista.");
+                        break;
+                }
+                Console.WriteLine("Presiona Enter para continuar...");
+                Console.ReadKey();
+                Console.Clear();
+                opcion = Presentacion();
+            }
+            Console.WriteLine("Has salido del menú");
+            Console.WriteLine("Presiona Enter para salir de la consola...");
+            Console.ReadKey();
+        }
+
+        static string Presentacion()
+        {
+            Console.WriteLine("Selecciona una opción");
+            Console.WriteLine("1.- Fibonacci");
+            Console.WriteLine("2.- Suma de Dígitos");
+            Console.WriteLine("3.- Encuentra numeros primos");
+            Console.WriteLine("4.- Ordenar frase");
+            Console.WriteLine("F.- Termina");
+            return Console.ReadLine();
+        }
+    }
+}
